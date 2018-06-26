aea0e20ff72e3488b7bbe6252031e84d5dda92fc
cgewecke <christophergewecke@gmail.com>
2018-06-25 18:32:46 -0700

    Install PLCR / Truffle V5

---
+++ truffle.js
@@ -0,0 +1,35 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+// first read in the secrets.json to get our mnemonic
+let secrets;
+let mnemonic;
+if (fs.existsSync('secrets.json')) {
+  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+  mnemonic = secrets.mnemonic;
+} else {
+  console.log('no secrets.json found. You can only deploy to the testrpc.');
+  mnemonic = '';
+}
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/${infura}`),
+      network_id: 4,
+      gas: 6000000,
+      gasPrice: 20000000000,
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infura}`),
+      network_id: 3,
+      gas: 4700000,
+      gasPrice: 20000000000,
+    },
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    }
+  },
+};
